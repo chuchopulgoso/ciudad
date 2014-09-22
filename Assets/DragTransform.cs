c9b0dc06842bb09a695412feda14449410bf14d8
nightsis <elena_siso@hotmail.com>
2014-09-22 20:31:46 +0200

    CrearMoverObjetos se fusiona con Master

--- Assets/DragTransform.cs
+++ Assets/DragTransform.cs
@@ -11,8 +11,8 @@ class DragTransform : MonoBehaviour
 	Vector3 rayPoint;
 	Ray ray;
 	Ray rayOriginal;
-	
-	
+
+
 	void OnMouseEnter()
 	{
 		renderer.material.color = mouseOverColor;
@@ -36,30 +36,7 @@ class DragTransform : MonoBehaviour
 
 	void OnCollisionEnter(Collision collision)
 	{
-		var rotationVector = transform.rotation.eulerAngles;
-		rotationVector.x = 0;
-		rotationVector.y = 0;
-		rotationVector.z = 0;
-
-		var positionVector = transform.position;
-		positionVector.x = 0;
-		positionVector.y = 0;
-		positionVector.z = 0;
-
-		if (collision.gameObject.name == "Terrain")
-		{ 
-			if(gameObject.name == "Street1Lane30m")
-			{
-				rotationVector.x = 270;
-			}
-			transform.rotation = Quaternion.Euler(rotationVector); 
-		} 
-		if (collision.gameObject.name == "Block_Building_Low")
-		{ 
-			renderer.material.color = Color.red;
-			dragging = false;
-		} 
-		if (collision.gameObject.name == "Street1Lane30m")
+		if ((collision.gameObject.name == "Edificio1(Clone)") || collision.gameObject.name == "ViaHorizontal(Clone)")
 		{ 
 			renderer.material.color = Color.red;
 			dragging = false;
@@ -74,10 +51,7 @@ class DragTransform : MonoBehaviour
 			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 			rayOriginal = ray;
 			rayPoint = ray.GetPoint(distance);
-			if (rayPoint.y < 0)
-			{
-				rayPoint.y = 0;
-			}
+			rayPoint.y = 0;
 			transform.position = rayPoint;
 		}
 	}
